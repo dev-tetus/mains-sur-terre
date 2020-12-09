452c41362a1a30994e31f46439601d5819994a10
Teo Roca <teo.roca798@gmail.com>
2020-12-09 19:02:12 +0100

    styling

--- client/src/APropos.js
+++ client/src/APropos.js
@@ -4,9 +4,8 @@ import Nav from "./components/Nav/Nav.js";
 function APropos() {
   return (
     <div>
+      <Nav />
       <h1>a propos page</h1>
-
-      <Nav/>
     </div>
   );
 }
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,19 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Nav.css";
 import { Link } from "react-router-dom";
-import { RiShoppingBag2Line } from "react-icons/ri";
-import MenuIcon from "../../iconComponents/MenuIcon.js";
+import { RiShoppingBag2Line, RiContactsBookLine } from "react-icons/ri";
+import { BiCartAlt } from "react-icons/bi";
+import { GiInspiration } from "react-icons/gi";
+import gsap from "gsap";
+
+import Vase from "../../iconComponents/Vase.js";
+
 function Nav() {
+  const [open, setOpen] = useState(false);
+  function handleClick() {
+    const tl = gsap.timeline({
+      defaults: {
+        duration: 1,
+      },
+    });
+    if (open) {
+      setOpen(false);
+      tl.to(".links", {
+        opacity: 0,
+      });
+    } else {
+      setOpen(true);
+      tl.to(".links", {
+        opacity: 1,
+      });
+    }
+
+    console.log("hello world");
+  }
+
   return (
     <div className="nav-container">
       <div className="icon-menu">
-        <MenuIcon />
+        <Vase onClick={handleClick} className="menu" />
       </div>
       <div className="links">
-        <Link className="link" to="/boutique">
-          <RiShoppingBag2Line className="icon" />
-        </Link>
+        <div className="link-container">
+          <Link className="link" to="/contact">
+            <RiContactsBookLine className="icon" />
+          </Link>
+        </div>
+
+        <div className="line"></div>
+        <div className="link-container">
+          <Link className="link" to="/boutique">
+            <RiShoppingBag2Line className="icon" />
+          </Link>
+        </div>
+        <div className="line"></div>
+        <div className="link-container">
+          <Link className="link" to="/inspiration">
+            <GiInspiration className="icon" />
+          </Link>
+        </div>
       </div>
+      <BiCartAlt className="cart" />
     </div>
   );
 }
