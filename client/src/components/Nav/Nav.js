3b02ad2e5ca99220b96dcf245ba91aa9f4c985c8
Teo Roca <teo.roca798@gmail.com>
2020-12-09 16:11:27 +0100

    new files added

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,7 +1,21 @@
 import React from "react";
 import "./Nav.css";
+import { Link } from "react-router-dom";
+import { RiShoppingBag2Line } from "react-icons/ri";
+import MenuIcon from "../../iconComponents/MenuIcon.js";
 function Nav() {
-  return <div></div>;
+  return (
+    <div className="nav-container">
+      <div className="icon-menu">
+        <MenuIcon />
+      </div>
+      <div className="links">
+        <Link className="link" to="/boutique">
+          <RiShoppingBag2Line className="icon" />
+        </Link>
+      </div>
+    </div>
+  );
 }
-import "./Nav.css";
+
 export default Nav;
