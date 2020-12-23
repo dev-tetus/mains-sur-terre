2776b64474e1b129b3ac1b73ecef785f135a8faf
Teo Roca <teo.roca798@gmail.com>
2020-12-23 08:33:12 +0100

    credenciales añadidas

---
+++ backend/config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  DB_HOST: process.env.HOSTNAME,
+  DB_USER: process.env.USERNAME,
+  DB_PASSWORD: process.env.PASSWORD,
+  DB_NAME: process.env.DATABASE,
+};
--- backend/dbconfig.js
+++ backend/dbconfig.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql");
 const dotenv = require("dotenv");
-dotenv.config();
+const keys = require("./config/prod");
+
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: keys.DB_HOST,
+  user: keys.DB_USER,
+  password: keys.DB_PASSWORD,
+  database: keys.DB_NAME,
 });
 
 db.getConnection((err, connection) => {
