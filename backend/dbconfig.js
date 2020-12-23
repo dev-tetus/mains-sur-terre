29e32a4e8483a4c7fa7fef0d510faff4f2bbae68
Teo Roca <teo.roca798@gmail.com>
2020-12-23 09:23:01 +0100

    config bbdd

--- backend/dbconfig.js
+++ backend/dbconfig.js
@@ -1,20 +1,19 @@
-const mysql = require("mysql");
+const mysqlx = require("@mysql/xdevapi");
 const dotenv = require("dotenv");
 const keys = require("./config/prod");
 
-const db = mysql.createPool({
+const config = {
   host: keys.DB_HOST,
   user: keys.DB_USER,
   password: keys.DB_PASSWORD,
   database: keys.DB_NAME,
-});
+};
 
-db.getConnection((err, connection) => {
-  if (err) throw err;
-  else {
-    console.log("Successfully connected!!");
-    connection.release();
-  }
+const client = mysqlx.getClient(config, {
+  pooling: { enabled: true, maxSize: 10 },
+});
+client.getSession().then((session) => {
+  console.log(session.inspect());
 });
 
 module.exports = { db };
