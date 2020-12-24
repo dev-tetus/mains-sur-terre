5fbaaa93b16e47fec32affe0640e7ef6d298b59b
dev-tetus <teo.roca798@gmail.com>
2020-12-24 02:22:13 +0100

    añadido middleware rutas productos y conexion bbdd

--- backend/index.js
+++ backend/index.js
@@ -2,36 +2,29 @@ const express = require("express");
 
 const app = express();
 const cors = require("cors");
-const db = require("./config");
 require("dotenv").config();
+
+const pool = require('./dbconfig')
+
+const products = require('./routes/products');
+
+app.use("/products", products);
 
 const port = process.env.PORT || 3001;
 
 app.use(cors());
 
+
+
+pool.getConnection((err, connection) => {
+  if (err) throw err;
+  else console.log("Sucessfully connected!");
+  connection.release();
+})
+
+
+
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
 
-app.get("/api", (req, res) => {
-  res.send("Hello world");
-});
-
-app.get("/produits", (req, res) => {
-  const selectStatement = "select * from produits ";
-  db.query(selectStatement, (err, result) => {
-    if (err) {
-      throw err;
-    } else res.send(result).json;
-  });
-});
-
-app.get("/produits/insert", (req, res) => {
-  const insertStatement =
-    "INSERT INTO produits(name, description, price) VALUES('newName', 'amazing stuff', 23)";
-  db.query(insertStatement, (err, result) => {
-    if (err) {
-      throw err;
-    } else res.send("Succesfully added!!");
-  });
-});
