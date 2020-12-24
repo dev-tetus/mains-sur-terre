ba7169dcfe75c8cd9d082f1f1296f57843b1cf1f
Teo Roca <teo.roca798@gmail.com>
2020-12-24 15:13:37 +0100

    refactor code

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,28 @@
 const express = require("express");
-
 const app = express();
 const cors = require("cors");
+const bodyParser = require("body-parser");
+
 require("dotenv").config();
 
-const pool = require('./dbconfig')
+const pool = require("./dbconfig");
 
-const products = require('./routes/products');
+const products = require("./routes/products");
+console.log(process.env.NODE_ENV);
 
 app.use("/products", products);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = process.env.PORT || 3001;
-
-app.use(cors());
-
-
 
 pool.getConnection((err, connection) => {
   if (err) throw err;
   else console.log("Sucessfully connected!");
   connection.release();
-})
-
-
+});
 
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
-
