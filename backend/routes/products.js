72636d2c1bd0b7351ec64a197e1d38f7cf28284c
Teo Roca <teo.roca798@gmail.com>
2020-12-26 23:21:49 +0100

    refactoriza codigo e importa esquemas validacion

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bodyParser = require("body-parser");
 const pool = require("../dbconfig");
-const Joi = require('joi');
+const Joi = require("joi");
+const productSchema = require("../Helpers/products_schema");
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
@@ -14,7 +15,7 @@ router.get("/", (req, res) => {
     if (err) throw err;
     connection.query(selectQuery, (err, result) => {
       res.send({ result });
-      connection.release();
+      connection.destroy();
       if (err) throw err;
     });
   });
@@ -22,96 +23,90 @@ router.get("/", (req, res) => {
 
 //* Get one product from db from a given id
 router.get("/:id", async (req, res) => {
-  const id = req.params.id;
-  console.log(id);
-  //* Validate params and catch error in case of not valid
   try {
-    const validate = await Joi.string().regex(/^\d+$/).required().validateAsync(id);
+    const validId = await productSchema.regexId.validateAsync(req.params.id);
     const getProductQuery = "SELECT * FROM produits WHERE id = ?";
 
     pool.getConnection((err, connection) => {
-      if (err) throw err;
-      connection.query(getProductQuery, id, (error, result) => {
+      if (err) res.send(err).status(500);
+      connection.query(getProductQuery, validId, (error, result) => {
         if (Object.keys(result).length === 0) res.sendStatus(404);
         else {
-          res.send(result);
+          res.send(result).status(200);
         }
-      })
+        connection.destroy();
+      });
     });
   } catch (e) {
     if (e.isJoi === true) res.sendStatus(422);
-
   }
-})
+});
 
 //*Insert one unique product to database
-//TODO: Validate data with Joi and do try catch
-router.post("/insert", (req, res) => {
-  const name = req.body.name;
-  const description = req.body.description;
-  const price = req.body.price;
-
-
-  const selecteUniqueQuery =
-    "INSERT INTO produits (name, description, price)  VALUES (?,?,?)";
-  pool.getConnection((err, connection) => {
-    if (err) throw err;
-    connection.query(selecteUniqueQuery, [name, description, price], (error, result) => {
-      res.status(200).send(result);
-      connection.release();
-      if (error) throw error;
-    }
-    );
-  });
+router.post("/insert", async (req, res) => {
+  try {
+    const validData = await productSchema.insertProduct.validateAsync(req.body);
+    const selectUniqueQuery =
+      "INSERT INTO produits (name, description, price)  VALUES (?,?,?)";
+    pool.getConnection((err, connection) => {
+      if (err) res.send(err).status(500);
+      else {
+        connection.query(
+          selectUniqueQuery,
+          [validData["name"], validData["description"], validData["price"]],
+          (error, result) => {
+            if (error) res.send(error).status(500);
+            res.send(result).status(200);
+            connection.destroy();
+          }
+        );
+      }
+    });
+  } catch (e) {
+    if (e.isJoi === true) res.send(e);
+  }
 });
 
 //*Update one product to db with optional params
 //?Use of put instead of patch for the moment
-//TODO: Make schema on separated file
-router.put("/update/:id", (req, res) => {
-  //* SCHEMA for validation
-
-  const schema = Joi.object({
-    name: Joi.string().min(3).max(30),
-    description: Joi.string().min(5).max(500),
-    price: Joi.number().greater(0),
-  })
-
+router.put("/update/:id", async (req, res) => {
   try {
-    if (Object.keys(req.body).length === 0) {
-      res.send("no data");
-    }
-    else {
-      const updateQuery = "UPDATE produits SET ? WHERE id = ?";
-      const value = schema.validate(req.body);
-      pool.getConnection((err, connection) => {
-        if (err) throw err;
-        connection.query(updateQuery, [req.body, req.params.id], (error, result) => {
-          res.status(200).send(result)
-          connection.release();
-          if (error) throw error;
-        })
-      })
-
-    }
+    const validData = await productSchema.updateProduct.validateAsync(req.body);
+    const validId = await productSchema.regexId.validateAsync(req.params.id);
+    const updateQuery = "UPDATE produits SET ? WHERE id = ?";
+    pool.getConnection((err, connection) => {
+      if (err) res.send(err).status(500);
+      connection.query(updateQuery, [validData, validId], (error, result) => {
+        console.log(result);
+        if (result["affectedRows"] === 0) res.sendStatus(404);
+        else if (error) res.send(error).status(500);
+        else {
+          res.sendStatus(200);
+          connection.destroy();
+        }
+      });
+    });
   } catch (e) {
+    if (e.isJoi === true) res.sendStatus(422);
   }
-
 });
 
 //Delete product from database
-//TODO: Validate id data with Joi like router.get(/:id) and do try catch
-router.delete("/delete/:id", (req, res) => {
-  const deleteQuery = "DELETE FROM produits where id = ?";
-  pool.getConnection((err, connection) => {
-    if (err) throw err;
-    connection.query(deleteQuery, req.params.id, (error, result) => {
-      res.send(result);
-      console.log(result.affectedRows);
-      connection.release();
-      if (error) console.error(error.message);
-    })
-  })
-})
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const deleteQuery = "DELETE FROM produits where id = ?";
+    const validId = await productSchema.regexId.validateAsync(req.params.id);
+    pool.getConnection((err, connection) => {
+      if (err) throw err;
+      connection.query(deleteQuery, validId, (error, result) => {
+        res.sendStatus(200);
+        connection.destroy();
+        if (error) res.send(error).status(500);
+      });
+    });
+  } catch (e) {
+    if (e.isJoi === true) res.sendStatus(422);
+  }
+});
 
 module.exports = router;
