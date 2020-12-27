6a1313caa9ff92b9b5bebff8c6337bc997cd0d15
Teo Roca <teo.roca798@gmail.com>
2020-12-27 19:05:57 +0100

    añade esquema validacion login

--- backend/Helpers/auth_schema.js
+++ backend/Helpers/auth_schema.js
@@ -1,9 +1,17 @@
 const Joi = require("joi");
 
 const registerSchema = Joi.object({
+  email: Joi.string().email().required(),
   username: Joi.string().max(15).required(),
-  password: Joi.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/).required(),
-
+  password: Joi.string()
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+    )
+    .required(),
 });
-module.exports = {registerSchema 
-}
+const loginSchema = Joi.object({
+  user: Joi.string().max(30).required(),
+  password: Joi.string().max(60).required(),
+});
+
+module.exports = { registerSchema, loginSchema };
