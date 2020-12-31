1dc4503a6c89997c7ba421eb74629430f470b569
Teo Roca <teo.roca798@gmail.com>
2020-12-31 17:44:49 +0100

    reestructura archivos

--- backend/config/prod.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  DB_HOST: process.env.HOSTNAME,
-  DB_USER: process.env.USERNAME,
-  DB_PASSWORD: process.env.PASSWORD,
-  DB_NAME: process.env.DATABASE,
-  API_KEY_VERIFIER: process.env.API_KEY,
-};
