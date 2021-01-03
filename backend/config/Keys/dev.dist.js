ec8ec40c1fe7aa0d194b4f6a98b7bf081c21d1ab
Teo Roca <teo.roca798@gmail.com>
2021-01-03 19:37:42 +0100

    añade archivos genericos variables del entorno

---
+++ backend/config/Keys/dev.dist.js
@@ -0,0 +1,26 @@
+module.exports = {
+  PORT: "PORT",
+  DB: {
+    DB_HOST: "DB_HOST",
+    DB_USER: "DB_USER",
+    DB_PASSWORD: "DB_PASSWORD",
+    DB_NAME: "DB_NAME",
+  },
+  REDIS: {
+    PORT: "PORT",
+    HOST: "HOST",
+    PASSWORD: "PASSWORD",
+    SECRET_KEY:
+      "SECRET_KEY",
+    SESSION_NAME: "SESSION_NAME",
+  },
+  MAIL_VERIFIER: {
+    API_KEY: "API_KEY",
+  },
+  JWT: {
+    ACCESS_KEY:
+      "ACCESS_KEY",
+    REFRESH_KEY:
+      "REFRESH_KEY",
+  },
+};
