b1a50a89657c97aa8ba4da6df216c415dd682e91
Teo Roca <teo.roca798@gmail.com>
2021-01-03 19:00:23 +0100

    añade pagina registro

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,18 @@ import Nav from "./components/Nav/Nav.js";
 import Boutique from "./Boutique.js";
 import APropos from "./APropos.js";
 import Home from "./Home.js";
+import Register from "./Pages/Register/Register";
 
 function App() {
   return (
     <Router>
       <div className="app">
         <Switch>
-          
           <Route path="/" exact component={Home} />
           <Route path="/boutique" exact component={Boutique} />
           <Route path="/apropos" component={APropos} />
+          <Route path="/register" component={Register} />
+          
         </Switch>
       </div>
     </Router>
