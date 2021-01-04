9b82f0846035d62c43592c19cb22f8231d0962b2
Teo Roca <teo.roca798@gmail.com>
2021-01-04 20:30:41 +0100

    borra archivos borra los arhivos ya utilizados para empezar de nuevo con nuevas ideas

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,6 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Nav from "./components/Nav/Nav.js";
-import Boutique from "./Boutique.js";
-import APropos from "./APropos.js";
-import Home from "./Home.js";
-import Register from "./Pages/Register/Register";
-import Login from "./Pages/Login/Login";
-
 function App() {
   return (
     <Router>
