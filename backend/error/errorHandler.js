bddb727e74e573ab4cb2a3768ed303b194f642b3
Teo Roca <teo.roca798@gmail.com>
2021-01-14 13:21:33 +0100

    añade return

--- backend/error/errorHandler.js
+++ backend/error/errorHandler.js
@@ -2,7 +2,7 @@ const ErrorApi = require("./ErrorApi");
 
 module.exports = errorHandler = (error, req, res, next) => {
   if (error instanceof ErrorApi) {
-    res.status(error.code).json({
+    return res.status(error.code).json({
       message: error.message,
     });
   }
