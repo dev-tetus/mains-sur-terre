cc70b276e3e4d217b80590c10bfda509201d0332
Teo Roca <teo.roca798@gmail.com>
2021-01-14 11:27:23 +0100

    añade componentes Login.js y Home.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import  Home  from "./Pages/Home/home";
+import Login from "./Pages/Login/Login";
 
 function App() {
   return (
@@ -8,9 +10,9 @@ function App() {
       <div className="app">
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/boutique" exact component={Boutique} />
-          <Route path="/apropos" component={APropos} />
-          <Route path="/register" component={Register} />
+          {/* <Route path="/boutique" exact component={Boutique} />
+          <Route path="/apropos" component={APropos} /> */}
+          {/* <Route path="/register" component={Register} /> */}
           <Route path="/login" component={Login} />
         </Switch>
       </div>
